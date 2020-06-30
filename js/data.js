03de616900fd31545885a0f0ba476073cdb5be99
aripdevel <aripdevel@gmail.com>
2020-06-30 21:52:45 +0700

    upload file lokal pertama

---
+++ js/data.js
@@ -0,0 +1,393 @@
+/* Master Data */
+
+var section={
+    "home":{
+        "href":"#intro",
+        "text":"Beranda",
+        "content":{
+            "intro":[
+                {
+                    "background": "img/intro-carousel/bannerweb.jpg",
+                    "title": "Go Industry 4.0",
+                    "description": "Teknologi informasi merupakan sunnatullah dalam kehidupan masa kini.Siapa yang menguasai informasi,dialah yang akan bisa menaklukan dunia.Sebaliknya,orang akan mengalami keropatan jika menghindari dari teknologi informasi.",
+                    "target": "#featured-services"
+                }
+            ],
+            "social":[
+                {
+                    "platform":"github",
+                    "username":"@gudangsoft",
+                    "target":"https://github.com/Gudangsoft"
+                },
+                {
+                    "platform":"facebook",
+                    "username":"@gudangsoft.net",
+                    "target":"https://facebook.com/gudangsoft.net"
+                },
+                {
+                    "platform":"instagram",
+                    "username":"@gudangsoftnet.net",
+                    "target":"https://instagram.com/gudangsoftnet"
+                }
+            ]
+        }
+    },
+    "spesial":{
+        "href":"#featured-services",
+        "text":"Spesial",
+        "content":{
+                "mentor":{
+                    "title":"Mentoring",
+                    "target":"https://learning.gudangsoft.net",
+                    "icon":"ion-social-youtube-outline"
+                },
+           
+                "paket": {
+                    "title": "Domain dan hosting",
+                    "target": "https://domainhosting.gudangsoft.net",
+                    "icon": "ion-android-checkbox-outline"
+                },
+            "blog": {
+                "title": "News Update",
+                "target": "https://blog.gudangsoft.net",
+                "icon": "ion-ios-paper-outline"
+            }
+            
+        }
+    },
+    "about": {
+        "href": "#about",
+        "text": "Tentang",
+        "content":{
+            "title":"Tentang Kami",
+            "description":"Halo gengs, hari yang cerah untuk memulai dengan kami.Temukan pengalaman terbaik bersama kami, dengan tim yang profesional dibidang teknologi informasi.",
+            "featured":{
+                "desktop":{
+                    "title":"Desktop Apps",
+                    "description":"Fitur Aplikasi desktop",
+                    "icon":"ion-android-desktop",
+                    "thumbnail":"img/about/about-desktop.jpg",
+                    "feature":[
+                        "Support Linux and windows",
+                        "Tampilan Menarik",
+                        "Mudah di operasikan"
+                    ]
+                },
+                "mobile":{
+                    "title":"Mobile Apps",
+                    "description":"Fitur Aplikasi mobile",
+                    "icon":"ion-android-phone-portrait",
+                    "thumbnail":"img/about/about-mobile.jpg",
+                    "feature":[
+                        "Support Android and IOS"
+                    ]
+                },
+                "web":{
+                    "title":"Web Apps",
+                    "description":"Fitur Aplikasi web",
+                    "icon":"ion-ios-browsers-outline",
+                    "thumbnail":"img/about/about-web.jpg",
+                    "feature":[
+                        'Mobile Friendly',
+                        'Responsive Design',
+                        'SEO Optimize'
+                    ]
+                    
+              
+                }
+            }
+        }
+    },
+    "portfolio": {
+        "href": "#portfolio",
+        "text": "Portofolio",
+        "content":{
+            "title":"Portofolio",
+            "description":"Ini gengs beberapa hasil dari tantangan yang sudah kami selesaikan.",
+            "services":['desktop','mobile','web','wpthema'],
+            "clients":{
+                "Bengkah":{
+                    "name":"Bengkah.com",
+                    "logo":"img/clients/bengkahlogo.jpg",
+                    "image":"img/portfolio/bengkah.jpg",
+                    "service":"web"
+                },
+                "suarakeadilan":{
+                    "name":"suarakeadilan.net",
+                    "logo":"img/clients/suarakeadilan.net.jpg",
+                    "image":"img/portfolio/suarakeadilan.jpg",
+                    "service":"web"
+                },
+                "kabarkadinkendal": {
+                    "name": "kabarkadinkendal.com",
+                    "logo": "img/clients/kabarkadinkendal.com.jpg",
+                    "image": "img/portfolio/kabarkadinkendal.jpg",
+                    "service": "web"
+                },
+                "auliyakendal": {
+                    "name": "sekolahalamauliya.sch.id",
+                    "logo": "img/clients/sekolahalamauliya.sch.id.jpg",
+                    "image": "img/portfolio/sekolahalamauli.jpg",
+                    "service": "web"
+                },
+                "mismifsa2": {
+                    "name": "mismifsa2.com",
+                    "logo": "img/clients/mismifsa2.com.jpg",
+                    "image": "img/portfolio/mimiftahussalam.jpg",
+                    "service": "web"
+                },
+                "peradi": {
+                    "name": "peradikendal.id",
+                    "logo": "img/clients/peradikendal.com.jpg",
+                    "image": "img/portfolio/peradi.jpg",
+                    "service": "web"
+                },
+             
+                "sangkar": {
+                    "name": "sangkarjaya",
+                    "logo":"img/clients/bengkahlogo.jpg",
+                    "image": "img/portfolio/sangkarjaya_gudangsoftnet_eycoss_demak.jpg",
+                    "service": "web"
+                },
+                "masalikil": {
+                    "name": "ma-masalikilhuda.sch.id",
+                    "logo": "img/clients/hder-ijo.jpg",
+                    "image": "img/portfolio/gudangsoft-masalikilhuda-jepara.jpg",
+                    "service": "web"
+                    
+                },
+                "uniska": {
+                    "name": "Aplikasi Keuangan UNISKA",
+                    "logo":"img/clients/bengkahlogo.jpg",
+                    "image": "img/portfolio/uniska.jpg",
+                    "service": "desktop"
+                },
+                  "kasirone": {
+                    "name": "Kasir One",
+                    "logo":"img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/portfolio/kasir-one-gudangsoft.jpg",
+                    "service": "desktop"
+                },
+                "bomalaw": {
+                    "name": "bomalaw.id",
+                    "logo": "img/clients/logoboma.jpg",
+                    "image": "img/portfolio/boma.jpg",
+                    "service": "web"
+                },
+                "sukadi": {
+                    "name": "sukad1.id",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/portfolio/sukadi.jpg",
+                    "service": "web"
+                },
+                "ppbd": {
+                    "name": "Aplikasi PSB Online",
+                    "logo": "img/clients/logoboma.jpg",
+                    "image": "img/portfolio/sekolahonline.jpg",
+                    "service": "web"
+                },
+                "sman1salem": {
+                    "name": "SMAN1 Salem",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/portfolio/sma1salem-gudangsoft.jpg",
+                    "service": "web"
+                },
+                "wesata": {
+                    "name": "weesata",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/wp/weesata.jpg",
+                    "service": "wpthema"
+                },
+                "medika": {
+                    "name": "Medika",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/wp/medika.jpg",
+                    "service": "wpthema"
+                },
+                "showroom": {
+                    "name": "showroom",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/wp/showroom.jpg",
+                    "service": "wpthema"
+                },
+                "dealer": {
+                    "name": "Dealer",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/wp/dealer.jpg",
+                    "service": "wpthema"
+                },
+                "toko": {
+                    "name": "Toko Online",
+                    "logo": "img/clients/gudangsoft-eycoss2.png",
+                    "image": "img/wp/toko.jpg",
+                    "service": "wpthema"
+                },
+                 "amh": {
+                    "name": "AMHLawOffice",
+                    "logo": "img/clients/logoboma.jpg",
+                    "image": "img/portfolio/amh.jpg",
+                    "service": "web"
+                },
+                "kohd": {
+                    "name": "KOHD Demak",
+                    "logo": "img/clients/logoboma.jpg",
+                    "image": "img/portfolio/kohd.jpg",
+                    "service": "web"
+                },
+            }
+        }
+    },
+    "clients": {
+        "href": "#clients",
+        "text": "Klien",
+        "content": {
+            "title": "Klien",
+            "description": "Kawan kami yang sudah memberikan tantangan."
+        }
+    },
+    "team": {
+        "href": "#team",
+        "text": "Tim",
+        "content":{
+            "title":"Tim GS",
+            "description":"Tim terbaik kami bersama anda.",
+            "member":[
+                {
+                    "name":"Eko Siswanto",
+                    "position":"Chief Executive Officer",
+                    "social":{
+                        "twitter":"https://twitter.com/eycoss",
+                        "facebook":"https://facebook.com/eycoss",
+                        "instagram":"https://instagram.com/eycoss"
+                    },
+                    "avatar":"img/team/eko_siswanto-ceo.jpg"
+                },
+                {
+                    "name": "Bagus Sudirman",
+                    "position": "Visual & Android Programmer ",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/denmass.bagoes",
+                        "instagram": "https://www.instagram.com/masden_bagoes/?hl=id"
+                    },
+                    "avatar": "img/team/bagus_sudirman-visual_and_android_programmer.jpg"
+                },
+                {
+                    "name": "Muhammad Sholikhan",
+                    "position": "Graphic Designer",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/emsholy",
+                        "instagram": "https://www.instagram.com/shona_art/?hl=id"
+                    },
+                    "avatar": "img/team/muhammad_sholikhan-graphic_design.jpg"
+                },
+                {
+                    "name": "Zaenal Mustafa",
+                    "position": "Design Analyst",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/zaenal.mustofa.167",
+                        "instagram": "https://www.instagram.com/zm_tofu/?hl=id"
+                    },
+                    "avatar": "img/team/zaenal_mustofa-design_analyst.jpg"
+                },
+                {
+                    "name": "Maulidiah Zulfa",
+                    "position": "Customer Support",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/maulidiah.zulfa",
+                        "instagram": "https://www.instagram.com/maulidiah.zulfa/?hl=id"
+                    },
+                    "avatar": "img/team/maulidiah_zulfa-customer_support.jpg"
+                },
+                {
+                    "name": "Haris Ihsanil Huda",
+                    "position": "Web Programmer",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/harisxid",
+                        "instagram": "https://www.instagram.com/haris_xid/?hl=id"
+                    },
+                    "avatar": "img/team/Haris_ihsanil_huda-web_programmer.jpg"
+                },
+                {
+                    "name": "Ahmad Ashifuddin",
+                    "position": "Marketing Director",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/ahmad.ashifuddin",
+                        "instagram": "https://www.instagram.com/asiv_lindafaza/?hl=id"
+                    },
+                    "avatar": "img/team/Ahmad_ashifuddin-marketing.jpg"
+                },
+                {
+                    "name": "Arif Iskandar",
+                    "position": "Contributor",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "",
+                        "instagram": ""
+                    },
+                    "avatar": "img/team/arif_iskandar-contributor.jpg"
+                },
+                
+                  {
+                    "name": "M.Ibnu Shofa",
+                    "position": "Android Progammer",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/#",
+                        "instagram": "https://www.instagram.com/#"
+                    },
+                    "avatar": "img/team/ibnuok.jpg"
+                },
+                
+                {
+                    "name": "Bayu Abdur Rohman",
+                    "position": "Software Enginering",
+                    "social": {
+                        "twitter": "",
+                        "facebook": "https://www.facebook.com/abdurrohmanbayu",
+                        "instagram": "https://www.instagram.com/masbay736/?hl=id"
+                    },
+                    "avatar": "img/team/bayuok.jpg"
+                },
+                
+              
+                
+        
+                
+            ]
+        }
+    },
+    "contact":{
+        "href":"#contact",
+        "text":"Kontak",
+        "content":{
+            "title":"Kontak",
+            "description":"Jangan pernah ragu untuk menghubungi kami.",
+            "contact":[
+                {
+                    "type":"Alamat",
+                    "value": "<ul class='link-location'><li><a href='https://maps.google.com/?cid=8377817231756726044'><i class='ion-android-locate'></i> Bengkah, Wonesekar, Karangawen, Demak</a></li><li><a href='https://maps.app.goo.gl/5Sw68otv4JJTs9C3A'><i class='ion-android-locate'></i> Sulonjari, Bakalrejo, Guntur, Demak</a></li><li><a href='#'><i class='ion-android-locate'></i> Kaliwungu, Kendal</a></li></ul >",
+                    "icon":"ion-ios-location-outline"
+                },
+                {
+                    "type":"Whatsapp",
+                    "value":"<a href='https://wa.me/6285640236283'>085640236283</a>",
+                    
+                    "icon":"ion-social-whatsapp-outline"
+                },
+                {
+                    "type":"Email",
+                    "value":"gudangsoft.net@gmail.com",
+                    "icon":"ion-ios-email-outline"
+                }
+            ]
+        }
+    }
+}
+/* Tes data */
+
---
+++ js/setting.js
@@ -0,0 +1,329 @@
+/* Setting for dataset */
+
+//  Menu Section
+
+c_navMenu=document.querySelector('.nav-menu');
+
+nav_menu(c_navMenu);
+
+function nav_menu(selector){
+    
+    section_keys=Object.keys(section);
+
+    xhtml="";
+    section_keys.forEach(element => {
+        template = "<li><a href="+section[element].href+">"+section[element].text+"</a></li>"    
+        xhtml+=template;
+    });
+
+    selector.innerHTML=xhtml;
+    selector.children[0].setAttribute('class','menu-active');
+}
+
+// End Menu Section
+
+// Intro
+
+c_carouselInner=document.querySelector(".carousel-inner");
+
+xhtml="";
+
+section.home.content.intro.forEach(el=>{
+
+  xhtml="";
+  section.home.content.social.forEach(el=>{
+    template =`<a href="`+el.target+`" class="`+el.platform+`"><i class="fa fa-`+el.platform+` "></i><span> `+el.username+`</span></a>`;
+    xhtml+=template;
+  })
+  
+    template = `<div class="carousel-item active">
+            <div class="carousel-background"><img src="`+el.background+`" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+              <h2>`+el.title+`</h2>
+              
+              <div class="social-links home-social">
+              `+xhtml+`
+                </div>
+              </div>
+            </div>
+          </div>`;
+          xhtml=template;
+})
+
+c_carouselInner.innerHTML=xhtml;
+
+// End Intro
+
+// Featured-services
+
+c_featuredServiceHeader=document.querySelector('.featured-services-header');
+
+make_featured_service_content(c_featuredServiceHeader);
+
+function make_featured_service_content(selector){
+  
+xhtml="";
+  
+Object.keys(section.spesial.content).forEach(el=>{
+  
+
+  template =`<div class="col-sm-4 box">
+            <i class="`+section.spesial.content[el].icon+`"></i>
+            <h4 class="title"><a href="`+ section.spesial.content[el].target + `">` + section.spesial.content[el].title +`</a></h4>
+          </div>`;
+
+  xhtml+=template;
+  
+})
+
+selector.innerHTML=xhtml;
+ 
+}
+
+// End Featured-services
+
+// About
+c_sectionHeader=document.querySelector(".section-header");
+
+make_section_header(c_sectionHeader);
+
+function make_section_header(selector){
+
+    template=`<h3>`+section.about.content.title+`</h3>
+            <p>`+section.about.content.description+`</p>
+    `
+    selector.innerHTML=template;
+}
+
+c_aboutCols=document.querySelector('.about-cols');
+
+make_about_featured(c_aboutCols);
+
+function make_about_featured(selector){
+
+    featured=Object.keys(section.about.content.featured);
+
+    xhtml="";
+    featured.forEach(el=>{
+
+    xfhtml="";
+    function make_about_feature_list(el){
+      section.about.content.featured[el].feature.forEach(el=>{
+
+        template=`<li>`+el+`</li>`;
+        xfhtml+=template;
+      })
+    }
+
+    make_about_feature_list(el);
+    
+    template =`<div class="col-md-4">
+                <div class="about-col">
+                <div class="img">
+                    <img src="`+section.about.content.featured[el].thumbnail+`" alt="" class="img-fluid">
+                        <div class="icon"><i class="`+ section.about.content.featured[el].icon +`"></i></div>
+                        </div>
+                    <h2 class="title"><p>`+ section.about.content.featured[el].title +`</p></h2>
+                    <p>
+                    `+ section.about.content.featured[el].description +`
+                    </p>
+                    <ul class="about-feature-list">`+xfhtml+`
+                    </ul>
+                </div>
+                </div>`;
+
+        xhtml+=template;
+    })
+    selector.innerHTML=xhtml;
+}
+
+// End About
+
+// Portfolio
+
+c_portfolio_header=document.querySelector('.portfolio-header');
+
+make_portfolio_header(c_portfolio_header);
+
+function make_portfolio_header(selector){
+  template =` <h3 class="section-title">`+section.portfolio.content.title+`</h3>
+          <p>`+section.portfolio.content.description+`</p>`;
+
+  selector.innerHTML=template;
+}
+
+c_portfolioFilters=document.querySelector('#portfolio-flters');
+
+make_portfolio_filter(c_portfolioFilters);
+
+function make_portfolio_filter(selector){
+    
+    xhtml =`<li data-filter="*" class="filter-active">All</li>`;
+
+    section.portfolio.content.services.forEach(el=>{
+        template = `<li data-filter=".filter-`+el+`">`+el+`</li>`
+        xhtml+=template; 
+    })
+
+    selector.innerHTML=xhtml;
+}
+
+c_portfolioContainer=document.querySelector('.portfolio-container');
+
+make_portfolio_content(c_portfolioContainer);
+
+function make_portfolio_content(selector){
+
+    xhtml="";
+
+    client_content=Object.keys(section.portfolio.content.clients);
+
+    client_content.forEach(el=>{
+
+        template =`<div class="col-lg-4 col-md-6 portfolio-item filter-`+section.portfolio.content.clients[el].service+`">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="`+section.portfolio.content.clients[el].image+`" class="img-fluid" alt="">
+                <p class="link-details" title="More Details">`+ section.portfolio.content.clients[el].service +`</p>
+              </figure>
+
+              <div class="portfolio-info">
+              <a href="`+ section.portfolio.content.clients[el].image + `" class="link-preview" data-lightbox="portfolio" data-title="Web 1" title="Preview"> <h4>` + section.portfolio.content.clients[el].name+`</h4></a>
+                
+                <p> `+section.portfolio.content.clients[el].service+`</p>
+              </div>
+            </div>
+          </div>`;
+
+          xhtml+=template;
+ 
+    })
+    selector.innerHTML=xhtml;
+}
+
+c_clientsCorousel=document.querySelector('.clients-carousel');
+
+make_portfolio_clients(c_clientsCorousel);
+
+function make_portfolio_clients(selector){
+
+   client=Object.keys(section.portfolio.content.clients);
+
+   xhtml="";
+   client.forEach(el=>{
+
+       template =`<img src="`+section.portfolio.content.clients[el].logo+`" alt="">`
+       xhtml+=template;
+   })
+
+   selector.innerHTML=xhtml;
+   
+}
+
+// End Portfolio
+
+// Client
+
+c_clientsHeader=document.querySelector('.clients-header');
+
+make_clients_header(c_clientsHeader);
+
+function make_clients_header(selector){
+  template =`<h3>`+section.clients.content.title+`</h3>
+              <p>`+section.clients.content.description+`</p>`;
+
+  selector.innerHTML=template;
+}
+
+// End Client
+
+// Team
+
+c_teamHeader=document.querySelector(".team-header");
+
+make_team_header(c_teamHeader);
+
+function make_team_header(selector){
+
+    template=`<h3>`+section.team.content.title+`</h3>
+                <p>`+section.team.content.description+`</p>`;
+
+    selector.innerHTML=template;
+}
+
+c_teamMember=document.querySelector(".team-member");
+
+make_team_member(c_teamMember);
+
+function make_team_member(selector){
+    
+    xhtml="";
+
+    section.team.content.member.forEach(el=>{
+
+        empty_twitter=el.social.twitter=="" ? "style='display:none'" : el.social.twitter;
+        empty_facebook = el.social.facebook == "" ? "style='display:none'" : el.social.facebook;
+        empty_instagram = el.social.instagram == "" ? "style='display:none'" : el.social.instagram;
+        template =`<div class="col-lg-3 col-md-6">
+            <div class="member">
+              <img src="`+el.avatar+`" class="img-fluid" alt="">
+              <div class="member-info">
+                <div class="member-info-content">
+                  <h4>`+ el.name +`</h4>
+                  <span>`+ el.position +`</span>
+                  <div class="social">
+                    <a href="`+ el.social.twitter + `" ` + empty_twitter+` ><i class="fa fa-twitter"></i></a>
+                    <a href="`+ el.social.facebook + `"` + empty_facebook +`><i class="fa fa-facebook"></i></a>
+                    <a href="`+ el.social.instagram + `"` + empty_instagram +`><i class="fa fa-instagram"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+        
+          xhtml+=template;
+    })
+
+    selector.innerHTML=xhtml;
+}
+
+// End Team
+
+// Contact
+
+c_contactHeader=document.querySelector('.contact-header');
+
+make_contact_header(c_contactHeader);
+
+function make_contact_header(selector){
+    template=`<h3>`+section.contact.content.title+`</h3>
+              <p>`+section.contact.content.description+`</p>`;
+    selector.innerHTML=template;
+}
+
+c_contactInfo=document.querySelector('.contact-info');
+
+make_contact_content(c_contactInfo);
+
+function make_contact_content(selector){
+    xhtml="";
+
+    section.contact.content.contact.forEach(el=>{
+
+        template =`<div class="col-md-4">
+                <div class="contact-address">
+                  <i class="`+ el.icon +`"></i>
+                  <h3>`+el.type+`</h3>
+                  <p>`+el.value+`</p>
+                </div>
+              </div>`;
+
+        xhtml+=template;
+
+    })
+
+    selector.innerHTML=xhtml;
+}
+
+// End Contact
