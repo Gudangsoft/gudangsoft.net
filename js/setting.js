12fbe03cf46845e30982bc4a398667f65db407e5
aripdevel <aripdevel@gmail.com>
2020-07-03 07:51:34 +0700

    Refactoring issue #2, #close_issue2

--- js/setting.js
+++ js/setting.js
@@ -399,26 +399,18 @@ function make_portfolio_clients(selector){
    client=Object.keys(section.portfolio.content.clients);
 
    xhtml="";
-   test="";
    client.forEach(el=>{
 
      logo=section.portfolio.content.clients[el].logo;
       
      if(!logo==""){
 
-        template =`<img src="`+section.portfolio.content.clients[el].logo+`" alt="">`;
-        test+=logo+"\n";
-        
-     }else{
-       template="";
-     }
-     
-
+       template =`<img src="`+section.portfolio.content.clients[el].logo+`" alt="">`;
+       
        xhtml+=template;
-   })
-
-   console.log(test);
-   
+    }
+  
+   })   
 
    selector.innerHTML=xhtml;
    
